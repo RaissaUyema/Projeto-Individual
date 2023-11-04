e51268c371be70b84d6cb81a963287985642f770
Raissa Uyema <raissa.meggiolaro@sptech.school>
2023-11-04 18:46:02 -0300

    atualização do carrosel

--- index/javaScript.js
+++ index/javaScript.js
@@ -1,11 +1,15 @@
 let count = 1; 
-document.getElementById("radio1").checked = true;  
-setInterval( 
-    function() {     
-        nextImage(); }, 2000);  
+document.getElementById("radio1").checked = true; 
+
+setInterval( function() {     
+        nextImage(); 
+    }, 5000);  
+    
     function nextImage() {     
         count++     
         if(count > 4){     
             count = 1;     
         }      
-        document.getElementById("radio"+count).checked = true;  }
+
+        document.getElementById("radio"+count).checked = true;  
+    }
