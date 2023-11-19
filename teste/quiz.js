9ddb287a6fa872e0be65261e707dea700a9c8fff
Raissa Uyema <raissa.meggiolaro@sptech.school>
2023-11-19 18:37:35 -0300

    atualização do quiz

--- teste/quiz.js
+++ teste/quiz.js
@@ -1,1 +1,33 @@
- 
+   
+//matriz de objetos
+const quiz = [
+    {
+        q: 'Qual é o clã de Sasuke?',
+        opcao:['Uzumaki', 'Hyuga', 'Uchiha', 'Nara'],
+        resultado: 2
+    },
+
+    {
+        q: 'Qual é o nome do sensei de Naruto na Academia Ninja?',
+        opcao:['Jiraiya', 'Iruka', 'Kakashi', 'Tsunade'],
+        resultado: 1
+    },
+
+    {
+        q: 'Qual é o elemento de chakra principal de Naruto?',
+        opcao:['Fogo', 'Água', 'Vento', 'Trovão'],
+        resultado: 2
+    },
+
+    {
+        q: 'Quem é conhecido por ser o "Ninja Copiador"?',
+        opcao:['Kakashi Hatake', 'Sasuke Uchiha', 'Itachi Uchiha', 'Neji Hyuga'],
+        resultado: 0
+    },
+
+    {
+        q: 'O que significa o nome "Hokage"?',
+        opcao:['Sombra', 'Fogo', 'Trovão', 'Fogo de Deus'],
+        resultado: 3
+    }
+  ]
