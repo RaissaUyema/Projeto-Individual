1f313b7602f295637f621a6182a9234382cb28f7
Raissa Uyema <raissa.meggiolaro@sptech.school>
2023-11-22 19:52:53 -0300

    atualização do personagem

--- personagens/personagens.js
+++ personagens/personagens.js
@@ -6,25 +6,28 @@
  
     if(person == 'naruto'){
         imagem_informacao_person.innerHTML = `<img src="../imagem/narutooo.jpg" width="280px">`;
-        sobre_informacao_person.innerHTML =`loremloremloremloremloremloremloremloremloremloremloerme`
-        tipo_person.innerHTML = `jtnjnjnrjfnfjmfjnjugn`
+        sobre_informacao_person.innerHTML =``
+        tipo_person.innerHTML = ``
+
     } else if(person == 'sakura'){
         imagem_informacao_person.innerHTML = `<img src="../imagem/sakura.jpg" width="280px" alt="sakura">`;
-        sobre_informacao_person.innerHTML =`loremloremloremloremloremloremloremloremloremloremloerme`
-        tipo_person.innerHTML = `jtnjnjnrjfnfjmfjnjugn`
+        sobre_informacao_person.innerHTML =``
+        tipo_person.innerHTML = ``
+
     } else if(person == 'sasuke'){
         imagem_informacao_person.innerHTML = `<img src="../imagem/sasuke1.jpg" width="280px"></div>`;
-        sobre_informacao_person.innerHTML =`loremloremloremloremloremloremloremloremloremloremloerme`
-        tipo_person.innerHTML = `jtnjnjnrjfnfjmfjnjugn`
+        sobre_informacao_person.innerHTML =``
+        tipo_person.innerHTML = ``
+
     } else if(person == 'kakashi'){
         imagem_informacao_person.innerHTML = `<img src="../imagem/kakashi.jpg" width="280px"></div>`;
-        sobre_informacao_person.innerHTML =`loremloremloremloremloremloremloremloremloremloremloerme`
-        tipo_person.innerHTML = `jtnjnjnrjfnfjmfjnjugn`
+        sobre_informacao_person.innerHTML =``
+        tipo_person.innerHTML = ``
     }
     else if(person == 'jiraiya'){
         imagem_informacao_person.innerHTML = `<img src="../imagem/ji.jpg" width="280x"></div>`;
-        sobre_informacao_person.innerHTML =`loremloremloremloremloremloremloremloremloremloremloerme`
-        tipo_person.innerHTML = `jtnjnjnrjfnfjmfjnjugn`
+        sobre_informacao_person.innerHTML =``
+        tipo_person.innerHTML = ``
     }
 
 
@@ -33,15 +36,19 @@
 
 function fechar_modal(){
     var modal_person = document.getElementById("modal_informacao_person");
+
+    modal_person.style.display = 'none';
 }
 
 window.addEventListener('click', (e) => {
-    var modal_tipos = document.getElementById("modal_informacao_person");
-    if (e.target == modal_tipos ){
+    var modal_person = document.getElementById("modal_informacao_person");
+    if (e.target == modal_person ){
 
-        fechar_modais()
+        fechar_modal()
     }
 }
 )
 
-// modal_tipo_vinhos
+function favoritos(){
+    
+}
